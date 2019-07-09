86c8e1d7e6983820e331df99e7fc24f15e61194a
Leo040832 <52515434+Leo040832@users.noreply.github.com>
2019-07-09 15:06:48 +0800

    Add files via upload

--- portfolio/labs/lab03/lab.js
+++ portfolio/labs/lab03/lab.js
@@ -1,57 +1,66 @@
 /*
 	https://www.html5rocks.com/en/tutorials/file/dndfiles/
 */
+function readFiles(files){
 
-
-function readFiles(files) {
-
-    var output=[];
-    for(var i=0,f;f=files[i];i++){
-        output.push('<li><strong>',escape(f.name),'</strong> (',f.type||'n/a',')-',
-            f.size,'bytes,last modified:',
-            f.lastModifiedDate ?f.lastModifiedDate.toLocaleDateString():'n/a',
+var output = [];
+for(var i = 0,f; f = files[i]; i++){
+ output.push('<li><strong>',escape(f.name), '</strong> (',f.type||'n/a', ')-',
+            f.size, 'bytes, last modified:',
+            f.lastModifiedDate ? f.lastModifiedDate.toLocaleDateString():'n/a',
             '</li>');
-    }
-document.getElementById('list').innerHTML='<ul>'+output.join('')+'</ul>';
+}
+document.getElementById('list').innerHTML='<ul>' + output.join('') + '</ul>';
+document.getElementById('content').innerHTML = '';
+document.getElementById('svgimage').innerHTML = '';
 
-document.getElementById('content').innerHTML='';
-document.getElementById('svgimage').innerHTML='';
-
-for(var i=0,f;f=files[i];i++){
-    var reader=new FileReader();
+for(var i = 0, f; f = files[i];i++){
+    var reader = new FileReader();
     reader.readAsText(files[i],'UTF-8');
 
-    reader.onload=function(evt){
-        var span=document.createElement('span');
+    reader.onload = function(evt){
+        var span = document.createElement('span');
         span.setAttribute("class","svgshow");
-        span.innerHTML=evt.target.result;
+        span.innerHTML = evt.target.result;
         document.getElementById('svgimage').insertBefore(span,null);
 
-        var span=document.createElement('span');
+        var span = document.createElement('span');
         span.setAttribute("class", "svgtext");
-        span.textContent=evt.target.result;
-        document.getElementById('content').insertBefore(span,null)
+        span.textContent = evt.target.result;
+        document.getElementById('content').insertBefore(span,null);
+
     }
 }
+
 }
+
 function handleFileSelect(evt){
-var files=evt.target.files;
-
-readFiles(files);
+    var files = evt.target.files;
+    readFiles(files);
 }
 
-function handFileSelect_drag(evt){
+function handleFileSelect_drag(evt){
     evt.stopPropagation();
     evt.preventDefault();
-    var files=evt.dataTransfer.files;
+    var files = evt.dataTransfer.files;
 
     readFiles(files);
 }
-function start(e){
+
+function handleDragOver(evt){
+    evt.stopPropagation();
+    evt.preventDefault();
+    evt.dataTransfer.dropEffect = 'copy';
+}
+
+function start(e) {
     document.getElementById('files').addEventListener('change',handleFileSelect,false);
-    var dropZone=document.getElementById('drop_zone');
-   dropZone.addEventListener('dragover', handleDragOver,false);
-   dropZone.addEventListener('drop', handleFileSelect_drag,false);
+    
+    var dropZone = document.getElementById('drop_zone');
+    dropZone.addEventListener('dragover',handleDragOver,false);
+    dropZone.addEventListener('drop', handleFileSelect_drag, false);
 }
 
 window.addEventListener( "load", start, false );
+
+
